d3531fddd6bc2682029368f4bf065faf7879d43c
Adrian Utomo <adrianhakimutomo@gmail.com>
2024-06-04 12:36:58 +0700

    chore: change title

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import ChatbotContainer from './components/ChatbotContainer.vue';
 <template>
   <div class="w-screen h-screen background-pattern">
     <div className="w-full h-full flex flex-col justify-center items-center gap-[20px] text-center">
-      <h1 className="text-black font-bold text-7xl">Tugas Akhir Chatbot Swarm Made with Vue</h1>
+      <h1 className="text-black font-bold text-7xl">Tugas Akhir Chatbot Swarm</h1>
       <p className="text-xl text-slate-800">Click on the bottom right icon to toggle the Chatbot Interface</p>
     </div>
     <Suspense>
